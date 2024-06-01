029634afb87f9ff471f6770c379f4a346332b770
Aryan yadav <88836722+aryan0809@users.noreply.github.com>
2024-06-01 21:32:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,7 @@ const userRoute= require('./routes/userRoute');
 const mongoose = require('mongoose');
 const User = require('./models/userModel');
 const cors= require('cors');
-app.use(cors(
-    {
-        origin:["https://deploy-mern-1whq.vercel.app"],
-        methods: ["POST", "GET", "PATCH"],
-        credentials: true
-    }
-));
+app.use(cors());
 
 mongoose.connect("mongodb+srv://harsharyan1710:yl2vGBDJsxi9YRH1@cluster0.ce3gj.mongodb.net/curdMern", {
     useNewUrlParser: true,
