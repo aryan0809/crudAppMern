7290f864573561c3e661de27cb0dfb4c62eb5e9f
Aryan yadav <88836722+aryan0809@users.noreply.github.com>
2024-06-01 14:46:29 +0530

    Update Create.jsx

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { AvatarGenerator } from 'random-avatar-generator';
 
 function Create() {
