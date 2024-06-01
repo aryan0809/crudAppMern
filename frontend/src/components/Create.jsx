19b8f12035b169030e12faa9b0678a8dfc80f74e
Aryan yadav <88836722+aryan0809@users.noreply.github.com>
2024-06-01 14:40:57 +0530

    Update Create.jsx

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -18,8 +18,7 @@ function Create() {
         const profilePic = generator.generateRandomAvatar();
         const addUser = { name, email, age, profilePic };
 
-        const response = await fetch("crud-app-mern-gray.vercel.app
-/user", {
+        const response = await fetch("https://crud-app-mern-gray.vercel.app/user", {
             method: 'POST',
             body: JSON.stringify(addUser),
             headers: {
