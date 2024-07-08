c6c5c6c89c88e5a38c2801d75b70cde19d809b26
Aryan yadav <88836722+aryan0809@users.noreply.github.com>
2024-07-08 07:58:03 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,7 +20,9 @@ function Navbar() {
         </div>
         <div className="dropdown navbar-end lg:hidden">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-50">
             <li><Link to="/create">Create Post</Link></li>
@@ -34,3 +36,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
